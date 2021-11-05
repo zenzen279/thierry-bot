51135a6c7d0bf03dad6878bb78350093ed179fda
Fassi Théo <gtheya@gmail.com>
2021-11-05 19:48:37 +0100

    final ver
    Co-authored-by: fmaret <fmaret@users.noreply.github.com>

--- readWords.py
+++ readWords.py
@@ -4,3 +4,12 @@ def readWords(path: str = "./words.txt"):
             return file.read()
     except:
         return ""
+
+import json
+
+def readWordsJSON(path: str = "./mots.json"):
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except:
+        return ""
