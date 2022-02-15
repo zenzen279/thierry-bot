6aa831fa8a96e7f16ddfd5e1bc05025a8c1c2b15
fmaret <maret.florent59@gmail.com>
2022-02-15 22:25:29 +0100

    feat : update test with scraping

--- test.py
+++ test.py
@@ -1,25 +1,40 @@
-from pytrends.request import TrendReq
-from src.readWords import readWordsJSON
+# from pytrends.request import TrendReq
+# from src.readWords import readWordsJSON
 
-pytrends = TrendReq(hl='fr', tz=360) 
+# pytrends = TrendReq(hl='fr', tz=360) 
 
 
-def getResult(words):
-    kw_list = words # list of keywords to get data 
+# def getResult(words):
+#     kw_list = words # list of keywords to get data 
 
-    a = pytrends.build_payload(kw_list, cat=0, timeframe='today 12-m', geo="FR") 
+#     a = pytrends.build_payload(kw_list, cat=0, timeframe='today 12-m', geo="FR") 
 
-    data = pytrends.interest_over_time() 
-    data = data.reset_index() 
-    #print(data)
-    if data.empty:
-        return 0
-    return (data[kw_list].sum())
+#     data = pytrends.interest_over_time() 
+#     data = data.reset_index() 
+#     #print(data)
+#     if data.empty:
+#         return 0
+#     return (data[kw_list].sum())
 
-words = readWordsJSON("../public/words.json")
+# words = readWordsJSON("../public/words.json")
 
-words = words[0:100]
+# words = words[0:100]
 
-for i in range(len(words)):
-    words_cut = words[i:max(len(words), i+1)]
-    print(getResult(words_cut))
+# for i in range(len(words)):
+#     words_cut = words[i:max(len(words), i+1)]
+#     print(getResult(words_cut))
+
+import requests
+from bs4 import BeautifulSoup
+
+word = "gord"
+try:
+    url = f"https://fr.wiktionary.org/wiki/{word}"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    s = soup.find("ol").find_all("li")
+    print(s[0].getText())
+except:
+    print("Zé pas trouvé désolé")
+
+
